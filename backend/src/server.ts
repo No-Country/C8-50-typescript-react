4191585b91dee47aea84d86fb33fa760d64d95bb
ortega-pablo <p.ortegariera@gmail.com>
2022-11-14 22:23:16 -0300

    feat: inicialización de servidor

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+class Server {
+    public app: express.Application = express()
+    private port: number = 3000
+
+    constructor() {
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({ extended: true }))
+        this.app.use(morgan('dev'))
+        this.app.use(cors())
+
+       /*  this.app.use('/api', this.routes()) */
+
+        this.listen()
+    }
+
+    /* routes(): Array<express.Router> {
+        return []
+    } */
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server is listening on port ${this.port}`)
+        })
+    }
+
+}
+
+new Server()
