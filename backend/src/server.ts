c132145e15e625108309ba64009e8f77edde62b4
ortega-pablo <p.ortegariera@gmail.com>
2022-11-17 09:20:49 -0300

    feat: agrego variables de entorno

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import { UserRouter } from './routes/user.router'
+import { ConfigServer } from './config/config'
 
-class Server {
+class Server extends ConfigServer{
     public app: express.Application = express()
-    private port: number = 3000
+    private port: number = this.getNumberEnv("PORT")
 
     constructor() {
+        super()
         this.app.use(express.json())
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(morgan('dev'))
