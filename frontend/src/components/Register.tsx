2178a524034b9054b91477a427dc64cc840c2bd9
owenunda <oweenunda@gmail.com>
2022-11-20 09:07:46 -0500

    fix: changed to styled-components

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,8 +1,8 @@
 
 import { useForm } from "react-hook-form"
-import "../styled-components/register-card.css"
 import logo from "../assets/img/logo.png"
 import { useState } from "react"
+import styled from "styled-components"
 
 
 interface user {
@@ -12,10 +12,108 @@ interface user {
   confirmPassword: string
 }
 
-const RegisterForm = (): any => {
-  const { handleSubmit, register, formState: { errors }, reset } = useForm<user>()
 
-  const onSubmit = handleSubmit((data: user)  => {
+const ContainerCard  = styled.div`
+background-color: #908c8ca8;
+width: 100%;
+height: 100%;
+position: fixed;
+top: 0;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+z-index: 11;`
+const Card = styled.div`
+width: 330px;
+	height: 550px;
+	background-color: #e6e9f7;
+	display: flex;
+	flex-direction: column;
+	/* align-items: center; */
+	justify-content: space-around;
+	box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.4);
+	border-radius: 10px;
+	position: relative;`
+const TitleRegistro = styled.h1`
+  text-align: center;
+  color: #45484a;
+  font-size: 1.9rem;`
+const LabelInput = styled.label`
+font-size: .9rem`
+const Close = styled.button`
+position: absolute;
+top: 13px;
+right: 17px;
+background: transparent;
+border: none;`
+const ImgLogo = styled.img`
+width: 96px;
+margin: 0 auto;`
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 1.5rem;`
+const Containerinput = styled.div`
+width: 90%;
+position: relative;
+display: flex;
+flex-direction: column;
+`
+const InputForm = styled.input`
+height: 35px;
+line-height: 30px;
+border-radius: 7px;
+border: none;
+padding-left: 10px;
+`
+const CambioLogin = styled.p`
+text-align: center;
+font-size: .9rem;`
+const SpanCambio = styled.span`
+color: #3d4cd7;
+font-weight: bold;`
+const ErrorInput = styled.p`
+font-size: .8rem;
+color: red;
+`
+const ButtonRegister = styled.button`
+background-color: #3d4cd7;
+width: 180px;
+/* padding: 34.66px; */
+height: 33.66px;
+color: white;
+font-size: 14px;
+font-weight: bold;
+border: none;
+border-radius: 7px;
+box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.2);`
+const ViewButton = styled.button`
+position: absolute;
+right: 10px;
+top: 22px;
+border: none;
+background: transparent;`
+const MaterialSymbolsoutlined = styled.span`
+font-variation-settings:
+'FILL' 0,
+'wght' 400,
+'GRAD' 0,
+'opsz' 48;
+`
+
+
+  const RegisterForm = (): any => {
+
+    
+    
+
+
+
+  const { handleSubmit, register, formState: { errors }, reset } = useForm<user>()
+  const onSubmit = handleSubmit((data: user) => {
     console.log(data);
     reset({
       name: "",
@@ -24,61 +122,57 @@ const RegisterForm = (): any => {
       confirmPassword: ""
     })
   })
-
   const [isVisiblePassword, setIsvisiblePassword] = useState(false)
-
   const viewPassword = (): void => {
     setIsvisiblePassword(!isVisiblePassword)
   }
 
 
   return (
-    <div className="container-card">
-      <div className="Card" >
-        <button className="Close" >
-      <span className="material-symbols-outlined">
-        close
-      </span>
-        </button>
-        <img className="img-logo" src={logo} alt="logo-inmobiliaria" />
-        <h1 className="title-registro" > Registro </h1>
-        <form className="Form" onSubmit={() => onSubmit } >
-          <div className="container-input" >
-            <label htmlFor="nombreInput">Nombre</label>
-            <input {...register("name", {
+    <ContainerCard className="container-card">
+      <Card className="Card" >
+        <Close className="Close" >
+          <MaterialSymbolsoutlined className="material-symbols-outlined">close</MaterialSymbolsoutlined>
+        </Close>
+        <ImgLogo className="img-logo" src={logo} alt="logo-inmobiliaria" />
+        <TitleRegistro className="title-registro" > Registro </TitleRegistro>
+        <Form className="Form" onSubmit={() => onSubmit} >
+          <Containerinput className="container-input" >
+            <LabelInput htmlFor="nombreInput">Nombre</LabelInput>
+            <InputForm {...register("name", {
               required: true
             })}
               type="text"
               id="nombreInput"
               placeholder="Nombre completo" />
-            {errors.name?.type === "required" && <p className="errorInput" > Necesitas insertar tu nombre </p>}
-          </div>
-          <div className="container-input" >
-            <label htmlFor="emailInput">Correo</label>
-            <input {...register("email", { required: true, pattern: /\S+@\S+\.\S+/ })} type="text" id="emailInput" placeholder="Example@gmail.com" />
-            {errors.email?.type === "required" && <p className="errorInput" > Necesitas insertar tu correo </p>}
-            {errors.email?.type === "pattern" && <p className="errorInput" > Escribir bien el correo </p>}
-          </div>
-          <div className="container-input" >
-            <label htmlFor="passwordInput">Contraseña</label>
-            <button className="viewButton" type="button" onClick={viewPassword} >
-              <span className="material-symbols-outlined">
+            {errors.name?.type === "required" && <ErrorInput className="errorInput" > Necesitas insertar tu nombre </ErrorInput>}
+          </Containerinput>
+          <Containerinput className="container-input" >
+            <LabelInput htmlFor="emailInput">Correo</LabelInput>
+            <InputForm {...register("email", { required: true, pattern: /\S+@\S+\.\S+/ })} type="text" id="emailInput" placeholder="Example@gmail.com" />
+            {errors.email?.type === "required" && <ErrorInput className="errorInput" > Necesitas insertar tu correo </ErrorInput>}
+            {errors.email?.type === "pattern" && <ErrorInput className="errorInput" > Escribir bien el correo </ErrorInput>}
+          </Containerinput>
+          <Containerinput className="container-input" >
+            <LabelInput htmlFor="passwordInput">Contraseña</LabelInput>
+            <ViewButton className="ViewButton" type="button" onClick={viewPassword} >
+              <MaterialSymbolsoutlined className="material-symbols-outlined">
                 {isVisiblePassword ? "visibility" : "visibility_off"}
-              </span>
-            </button>
-            <input {...register("password", { required: true })} type={isVisiblePassword ? "text" : "password"} id="passwordInput" placeholder="***********" />
-            {errors.password?.type === "required" && <p className="errorInput" > Necesitas insertar tu contraseña </p>}
-          </div>
-          <div className="container-input" >
-            <label htmlFor="ConfirmpasswordInput">Confirmar contraseña</label>
-            <input {...register("confirmPassword", { required: true })} type={isVisiblePassword ? "text" : "password"} id="ConfirmpasswordInput" placeholder="***********" />
-            {errors.confirmPassword?.type === "required" && <p className="errorInput" > Necesitas confirmar tu contraseña </p>}
-          </div>
-          <button type="submit" > Register </button>
-        </form>
-        <p className="CambioLogin" > ¿ Ya tienes cuenta?  <span> iniciar sesion </span> </p>
-      </div>
-    </div>
+              </MaterialSymbolsoutlined>
+            </ViewButton>
+            <InputForm {...register("password", { required: true })} type={isVisiblePassword ? "text" : "password"} id="passwordInput" placeholder="***********" />
+            {errors.password?.type === "required" && <ErrorInput className="errorInput" > Necesitas insertar tu contraseña </ErrorInput>}
+          </Containerinput>
+          <Containerinput className="container-input" >
+            <LabelInput htmlFor="ConfirmpasswordInput">Confirmar contraseña</LabelInput>
+            <InputForm {...register("confirmPassword", { required: true })} type={isVisiblePassword ? "text" : "password"} id="ConfirmpasswordInput" placeholder="***********" />
+            {errors.confirmPassword?.type === "required" && <ErrorInput className="errorInput" > Necesitas confirmar tu contraseña </ErrorInput>}
+          </Containerinput>
+          <ButtonRegister type="submit" > Register </ButtonRegister>
+        </Form>
+        <CambioLogin className="CambioLogin" > ¿ Ya tienes cuenta?  <SpanCambio> iniciar sesion </SpanCambio> </CambioLogin>
+      </Card>
+    </ContainerCard>
   )
 }
 
