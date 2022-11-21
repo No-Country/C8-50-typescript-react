f3c596ff0a4a8f7be6b76ecd6d6dd8ec84127fe7
owenunda <oweenunda@gmail.com>
2022-11-21 08:37:53 -0500

    fix: use styled components for layout

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,7 +1,23 @@
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 import logo from "../assets/img/logo.png"
-import "../styled-components/login-card.css"
+import {
+  ContainerCard,
+  Card,
+  TitleLogin,
+  LabelInput,
+  Close,
+  ImgLogo,
+  Form,
+  Containerinput,
+  InputForm,
+  CambioRegister,
+  SpanCambio,
+  ErrorInput,
+  ButtonLogin,
+  ViewButton,
+  MaterialSymbolsoutlined
+} from "../styled-components/styled.login"
 
 interface loginForm{
   email: string
@@ -27,40 +43,40 @@ const LoginForm = (): any => {
   }
 
   return (
-    <div className="container-card" >
-      <div className="login-card" >
-        <button type="button" className="close" >
-          <span className="material-symbols-outlined">close</span>
-        </button>
-        <img className="logo" src={logo} alt="logo-inmobiliaria" />
-        <h1 className="title-login" > Iniciar sesion </h1>
-        <form className="form" onSubmit={handleSubmit(onSubmit)} >
-          <div className="container-input">
-            <label htmlFor="emailInput">Correo</label>
-            <input
+    <ContainerCard className="container-card" >
+      <Card className="login-card" >
+        <Close type="button" className="close" >
+          <MaterialSymbolsoutlined className="material-symbols-outlined">close</MaterialSymbolsoutlined>
+        </Close>
+        <ImgLogo className="logo" src={logo} alt="logo-inmobiliaria" />
+        <TitleLogin className="title-login" > Iniciar sesion </TitleLogin>
+        <Form className="form" onSubmit={ handleSubmit(onSubmit)} >
+          <Containerinput className="container-input">
+            <LabelInput htmlFor="emailInput">Correo</LabelInput>
+            <InputForm
               type="text"
               id="emailInput"
               placeholder="Example@gmail.com"
               {...register("email", {required: true})} />
-              {errors.email?.type === "required" && <p className="errorInput" >Inserte el correo</p> }
-          </div>
-          <div className="container-input" >
-            <label htmlFor="passwordInput">Contraseña</label>
-            <button type="button" onClick={ viewPassword} className="button-visibility" >
-            <span className="material-symbols-outlined">{isVisibility ? "visibility" : "visibility_off"}</span>
-            </button>
-            <input
+              {errors.email?.type === "required" && <ErrorInput className="errorInput" >Inserte el correo</ErrorInput> }
+          </Containerinput>
+          <Containerinput className="container-input" >
+            <LabelInput htmlFor="passwordInput">Contraseña</LabelInput>
+            <ViewButton type="button" onClick={ viewPassword} className="button-visibility" >
+            <MaterialSymbolsoutlined className="material-symbols-outlined">{isVisibility ? "visibility" : "visibility_off"}</MaterialSymbolsoutlined>
+            </ViewButton>
+            <InputForm
               type={isVisibility ? "text" : "password"}
               id="passwordInput"
               placeholder="**********"
               {...register("password", {required: true})} />
-              {errors.password?.type === "required" && <p className="errorInput" >Inserte la contraseña</p> }
-          </div>
-          <button className="button-login" type="submit" > Iniciar sesion </button>
-        </form>
-        <p className="not-account" >¿No tienes cuenta? <span className="register_click" >Registrate</span> </p>
-      </div>
-    </div>
+              {errors.password?.type === "required" && <ErrorInput className="errorInput" >Inserte la contraseña</ErrorInput> }
+          </Containerinput>
+          <ButtonLogin className="button-login" type="submit" > Iniciar sesion </ButtonLogin>
+        </Form>
+        <CambioRegister className="not-account" >¿No tienes cuenta? <SpanCambio className="register_click" >Registrate</SpanCambio> </CambioRegister>
+      </Card>
+    </ContainerCard>
   )
 }
 
