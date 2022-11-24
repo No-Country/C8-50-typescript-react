9b9304faa27541d6a71a7ed3178553c9fbd6a6dd
armosque <armosqueral@yahoo.es>
2022-11-24 15:59:11 -0500

    Feat: Entidad-Visit

--- backend/src/entities/User.entity.ts
+++ backend/src/entities/User.entity.ts
@@ -1,5 +1,6 @@
+import { Visit } from './Visit.entity';
 import { RealState } from './RealState.entity';
-import { Entity, Column, OneToMany } from "typeorm"
+import { Entity, Column, OneToMany, ManyToMany, JoinTable } from "typeorm"
 import { BaseEntity } from '../config/base.entity'
 
 @Entity()
@@ -25,4 +26,13 @@ export class User extends BaseEntity {
 
     @OneToMany(()=>RealState, (realstate)=>realstate.user)
     realstate: RealState[]
+
+    @ManyToMany(()=>Visit, (visit)=>visit.user)
+    @JoinTable()
+    visit: Visit[]
+
+    
+
+
+
 }
