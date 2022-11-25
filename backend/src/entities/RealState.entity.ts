4b4862a436c46ea97abf816ed842157c42b2dc2f
fredy alberto varon guzman <fvaron@itfip.edu.co>
2022-11-25 15:37:00 -0500

    Crear-Entidad-Wishlist

--- backend/src/entities/RealState.entity.ts
+++ backend/src/entities/RealState.entity.ts
@@ -1,9 +1,10 @@
 import { Comment } from './Comment.entity';
 import { Visit } from './Visit.entity';
 import { User } from './User.entity';
-import { Entity, Column, ManyToOne, OneToMany, JoinColumn } from "typeorm"
+import { Entity, Column, ManyToOne, OneToMany, JoinColumn, ManyToMany, JoinTable } from "typeorm"
 import { BaseEntity } from '../config/base.entity'
 import { Call } from './Call.entity';
+import { Wishlist } from './Whislist.entity';
 
 @Entity()
 export class RealState extends BaseEntity {
@@ -29,5 +30,9 @@ export class RealState extends BaseEntity {
 
     @OneToMany(()=>Call, (call)=>call.realstate)
     call: Call[]
+
+    @ManyToMany(()=> Wishlist, (wishlistreal)=>wishlistreal.realstate)
+    @JoinTable()
+    wishlistreal: Wishlist
 
 }
