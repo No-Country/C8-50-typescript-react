baf6aa908721d6811cedcd8e1ed0ec73b0cf6306
owenunda <oweenunda@gmail.com>
2022-11-29 00:31:20 -0500

    feat: card propetary data

--- frontend/src/pages/PropiedadDetail.tsx
+++ frontend/src/pages/PropiedadDetail.tsx
@@ -1,4 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom"
+import { Text } from "../styled-components/styled.infoHome"
 
 import {
   TextNavbar,
@@ -6,7 +7,23 @@ import {
   NavbarRouter,
   MaterialSymbolsoutlined,
   NamePropiedad,
-  Main
+  Main,
+  DivIcons,
+  DivTitle,
+  Propertydata,
+  VerifiedProperty,
+  TitleData,
+  BasicInformation,
+  TypePropetary,
+  IDPropetary,
+  Number,
+  Container,
+  DivCotainer,
+  TitleTex,
+  PropiedadInfo,
+  Div,
+  Hr,
+  DivDirection
 } from "../styled-components/styled.pages/styled.propiedadesDetail"
 const PropiedadDetail = (): any => {
   const Navigate = useNavigate()
@@ -21,20 +38,79 @@ const PropiedadDetail = (): any => {
         <SpanNavbar> Casa en venta en Quesada 5500, Villa Urquiza, Capital Federal {id}  {/* esto es interactivo */} </SpanNavbar>
       </NavbarRouter>
       <Main>
-
         <NamePropiedad>
           <MaterialSymbolsoutlined className="material-symbols-outlined">home</MaterialSymbolsoutlined>
           Casa en venta en Quesada 5500, Villa Urquiza, Capital Federal {id}  {/* esto es interactivo */}
         </NamePropiedad>
-        
         <section className="Container imgs" >
 
         </section>
-
+        <section>
+          <Propertydata>
+            <DivTitle>
+              <VerifiedProperty>Propiedad verificada</VerifiedProperty>
+              <TitleData>Datos generales</TitleData>
+              <DivIcons>
+                <span className="material-symbols-outlined">
+                  share
+                </span>
+                <span style={{ color: "red" }} className="material-symbols-outlined">
+                  favorite
+                </span>
+                <span style={{ color: "#efb810" }} className="material-symbols-outlined">
+                  report
+                </span>
+              </DivIcons>
+            </DivTitle>
+            <Hr />
+            <BasicInformation>
+              <TypePropetary> Tipo de propiedad: <span style={{ fontWeight: "bold" }} > Casa {/* esto es dinamico */} </span> </TypePropetary>
+              <IDPropetary> Codigo de propeidad: <span style={{ fontWeight: "bold" }} >212445 {/* esto es dinamico */} </span>  </IDPropetary>
+            </BasicInformation>
+            <Hr />
+            <DivDirection>
+              <DivCotainer>
+                <TitleTex> La propiedad cuenta con:</TitleTex>
+                <Container>
+                  <span className="material-symbols-outlined">
+                    bathtub
+                  </span>
+                  <Number>2</Number>
+                  <Text>baños</Text>
+                </Container>
+                <Container>
+                  <span className="material-symbols-outlined">
+                    bed
+                  </span>
+                  <Number>6</Number>
+                  <Text>Habit.</Text>
+                </Container>
+                <Container>
+                  <span className="material-symbols-outlined">
+                    square_foot
+                  </span>
+                  <Number>344m²</Number>
+                  <Text>Totales</Text>
+                </Container>
+              </DivCotainer>
+              <Div>
+                <PropiedadInfo> Direccion: <span style={{ fontWeight: "normal" }} >CRA 63 N° 34 - 40 {/* dinamico */} </span> </PropiedadInfo>
+                <PropiedadInfo> Pisos:  <span style={{ fontWeight: "normal" }} >2 {/* dinamico */} </span> </PropiedadInfo>
+              </Div>
+            </DivDirection>
+            <Hr />
+            <p> Valor de propiedad: <span style={{ fontWeight: "bold" }} > 249.999$ {/* dinamico */} </span>  </p>
+            <Hr />
+            <Div>
+              <Number> Descripcion: </Number>
+              <Text> Cuenta con 116 metros distribuidos en sala y comedor, balcón, cocina integral, zona de ropas, 2 baños, 3 alcobas, 3 closet, piso en cerámica, parqueadero, cuarto útil, gabinetes, red de gas. Fácil acceso,
+                buenas rutas de transporte. </Text>
+            </Div>
+          </Propertydata>
+          
+        </section>
 
       </Main>
-
-
     </>
   )
 }
--- frontend/src/styled-components/styled.pages/styled.propiedadesDetail.ts
+++ frontend/src/styled-components/styled.pages/styled.propiedadesDetail.ts
@@ -26,10 +26,9 @@ export const SpanNavbar = styled.span`
 	cursor: pointer;
 `;
 
-
 export const Main = styled.main`
-padding: .5rem;
-`
+	padding: 0.5rem;
+`;
 
 export const NamePropiedad = styled.p`
 	display: flex;
@@ -38,9 +37,106 @@ export const NamePropiedad = styled.p`
 	align-items: center;
 	font-size: 1.1rem;
 	font-weight: bolder;
-  margin-top: 2rem;
+	margin-top: 2rem;
 `;
 
+export const DivIcons = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 90px;
+	align-items: center;
+	justify-content: center;
+	gap: 10px;
+`;
+
+export const DivTitle = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: end;
+`;
+export const Propertydata = styled.section`
+	border: 4px solid #3d4cd74d;
+	box-shadow: 0px 4px 4px 0px #00000040;
+	border-radius: 10px;
+	max-width: 700px;
+	padding: 0.5rem;
+`;
+export const VerifiedProperty = styled.p`
+	margin: 0;
+	color: #3d4cd7; ;
+`;
+export const TitleData = styled.h1`
+	margin: 0;
+	color: #000000bf;
+	font-size: 1.4rem;
+`;
+
+export const BasicInformation = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+`;
+export const TypePropetary = styled.p`
+	margin: 0;
+`;
+export const IDPropetary = styled.p`
+	margin: 0;
+`;
+export const Container = styled.div`
+	width: 45px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 2px;
+`;
+export const ContainerDiv = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
+export const DivCotainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 61px);
+	width: 183px;
+`;
+export const TitleTex = styled.p`
+	grid-column: span 3;
+	margin: 0;
+	margin-bottom: 1rem;
+`;
+export const Number = styled.p`
+	margin: 0;
+	font-weight: bold;
+`;
+export const Text = styled.p`
+	margin: 0;
+	background: linear-gradient(0deg, #000000, #000000),
+		linear-gradient(0deg, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75));
+	font-weight: bold;
+`;
+
+export const PropiedadInfo = styled.p`
+	font-weight: bold;
+	margin: 0;
+`;
+export const Div = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+`;
 export const MaterialSymbolsoutlined = styled.span`
 	font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;
+`;
+export const DivDirection = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
+export const Hr = styled.hr`
+	margin-bottom: 2rem;
 `;
