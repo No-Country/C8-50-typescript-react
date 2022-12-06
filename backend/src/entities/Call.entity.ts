155c82cae33a1d8857f8caa3677811178e111c4a
fredy alberto varon guzman <fvaron@itfip.edu.co>
2022-12-06 12:24:22 -0500

    Arreglo campo descripcion in call

--- backend/src/entities/Call.entity.ts
+++ backend/src/entities/Call.entity.ts
@@ -1,20 +1,19 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm'
-import { BaseEntity } from '../config/base.entity';
-import { RealEstate } from './RealEstate.entity';
-import { User } from './User.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { BaseEntity } from "../config/base.entity";
+import { RealEstate } from "./RealEstate.entity";
+import { User } from "./User.entity";
 
 @Entity()
-export class Call extends BaseEntity{
-    @Column()
-    date: Date
+export class Call extends BaseEntity {
+  @Column()
+  date: Date;
 
-    @Column()
-    description: string
+  @Column({ type: "text" })
+  description: string;
 
-    @ManyToOne(()=> User, (user)=> user.call)
-    user: User
+  @ManyToOne(() => User, (user) => user.call)
+  user: User;
 
-    @ManyToOne(()=> RealEstate, (realEstate)=> realEstate.call)
-    realEstate: RealEstate
-
+  @ManyToOne(() => RealEstate, (realEstate) => realEstate.call)
+  realEstate: RealEstate;
 }
